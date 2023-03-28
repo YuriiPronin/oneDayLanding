b8a53a0f6b234dbd2206f6513d4326714c32fb8d
Yurii Pronin <ProninYurij@i.ua>
2023-03-28 18:41:38 +0300

    not correct but deploy

--- src/components/HomePage/HideBody1/HideBody1.jsx
+++ src/components/HomePage/HideBody1/HideBody1.jsx
@@ -6,7 +6,16 @@ export const HideBody1 = () => {
   return (
     <div className="hideBody1">
       <div className='max1440'>
-        <div className="hideBody1__content">
+        <div className="blockRd">
+          <div className="blockRd__someContent">
+            <p>у нас Своё производство</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <button>оформить заказ</button>
+          </div>
+          <div className="blockRd__imageWrap blockRd__imageWrap1"><img src={require('../../../images/HomePage/fone.jpg')} alt="" /></div>
+          <div className="blockRd__imageWrap blockRd__imageWrap2"><img src={require('../../../images/HomePage/frontNew.png')} alt="" /></div>
+        </div>
+        {/* <div className="hideBody1__content">
           <div className="proba">
             <h3 className="hideBody1__content--title">
               У Нас Своё Производство
@@ -22,10 +31,10 @@ export const HideBody1 = () => {
         </div>
 
         <img
-          src={require('../../../images/HomePage/hidenPie.png')}
+          src={require('../../../images/HomePage/frontNew.png')}
           alt=""
           className="hidenPie"
-        />
+        /> */}
       </div>
     </div>
   );
